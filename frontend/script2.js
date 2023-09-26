4d5f8af45a3804cbf39926938c7bf752b58221dc
FeketeNemetZsofia <zsofia.nemet91@gmail.com>
2023-09-26 20:11:53 +0200

    IF practice in script2

--- frontend/script2.js
+++ frontend/script2.js
@@ -1,1 +1,16 @@
-console.log("scripts2")
+function gameShow(firstPlace, secondPlace) {
+    console.log("First place: "+ firstPlace);
+    console.log("Second place: "+ secondPlace);
+};
+gameShow("Nick", "Miles")
+
+function report(result) {
+    if (result == "Not Guilty") {
+        console.log("CONGRATULATIONS!")
+    }
+    else {
+        console.log("TOO BAD, TRY AGAIN!")
+    }
+}
+
+report("Not Guilty")
