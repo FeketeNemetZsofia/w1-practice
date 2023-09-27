628d2217e65ad0048cc5a4f93cd28f795651d3f5
FeketeNemetZsofia <zsofia.nemet91@gmail.com>
2023-09-27 12:53:00 +0200

    wednesday practices

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-function namePrinter(name) { // Kiírja a megadott nevet
-    console.log(name)
+let variable1 = "string"
+let variable2 = 2
+let variable3 = true
+let variable4 = null
+
+let variableArray = ["alma", "barack", "szilva", "vegyes"];
+let variableArray2 = [1, 2, 3];
+let variableArray3 = ["autó", true, 62, null];
+let variableArray4 = [variable1, variable2, variable3]
+
+console.log(variableArray3[2])
+
+variableArray2[0] = 9
+console.log(variableArray2) 
+
+console.log(variableArray3.length)
+
+let counter = 0
+while (counter <= 5) {
+    console.log(counter)
+    counter = counter + 1 // vagy counter ++
 }
 
-function marriedNameGenerator(fianceName, maidenName) { // Összeköti a jövendőbeli és a menyasszony leánykori nevét
-    let marriedName = fianceName + maidenName
-        return marriedName
+for (let counter = 0; counter <= 5; counter++) {
+    console.log(counter);   
 }
 
-namePrinter(marriedNameGenerator("Fekete-", "Német Zsófia"))
+for (let index = 0; index < variableArray.length; index++) {
+    console.log(variableArray[index])
+    
+}
