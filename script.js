2b1d702c13905ff1d58477589c6f5288430d0a26
FeketeNemetZsofia <zsofia.nemet91@gmail.com>
2023-09-29 14:57:09 +0200

    friday practice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+let x = 10
+let y = "text"
+let z = true
+
+let arr = ["one", "two", "three"]
+let arr2 = [1, 2, 3, 4]
+let arr3 = [true, false]
+
+let newArr = [x, y, z]
+console.log(newArr)
